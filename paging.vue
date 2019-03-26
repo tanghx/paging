ffc74fea975c13df924fd5d456d63dfc76efdc03
唐海潇 <tanghaixiao@leying365.com>
2019-03-26 16:13:04 +0800

    修改分页逻辑

--- paging.vue
+++ paging.vue
@@ -1,15 +1,15 @@
 <template>
   <!-- 分页 -->
   <div class="paging">
-    <button class="prev" @click="prePage" :disabled="current == 1">上一页</button>
+    <button class="prev" @click="prePage" :disabled="nowPage == 1">上一页</button>
     <div class="page-list">
-      <button class="page-item" @click="chosePage(1)" :class="{active: current == 1}">1</button>
+      <button class="page-item" @click="chosePage(1)" :class="{active: nowPage == 1}">1</button>
       <button class="page-item" v-if="ellipsis1">···</button>
-      <button class="page-item" v-cloak v-for="(item, index) in pageList" :key="index" :class="{active: item == current}" @click="chosePage(item)">{{item}}</button>
+      <button class="page-item" v-cloak v-for="(item, index) in pageList" :key="index" :class="{active: item == nowPage}" @click="chosePage(item)">{{item}}</button>
       <button class="page-item" v-if="ellipsis2">···</button>
-      <button class="page-item" v-cloak  @click="chosePage(pageLength)" :class="{active: current == pageLength}" v-if="pageLength != 1">{{pageLength}}</button>
+      <button class="page-item" v-cloak  @click="chosePage(pageLength)" :class="{active: nowPage == pageLength}" v-if="pageLength != 1">{{pageLength}}</button>
     </div>
-    <button class="next" @click="nextPage" :disabled="current == pageLength">下一页</button>
+    <button class="next" @click="nextPage" :disabled="nowPage == pageLength">下一页</button>
   </div>
 </template>
 
@@ -17,60 +17,76 @@
 export default {
   //获取父组件传递来的页数和当前页
   props:{
-    getPageLength:{
+    total:{
       type: Number,
       default: 1,
     },
-    getCurrent:{
+    pageNum:{
       type: Number,
       default: 1
+    },
+    pageSize: {
+      type: Number,
+      default: 10
     }
   },
-  data: function(){
+  data(){
     return{
       ellipsis1: false,
       ellipsis2: false,
-      pageLength: this.getPageLength,     //总页数
-      current: this.getCurrent            //当前页
     }
   },
+  mounted(){
+    
+  },
   computed:{
+    pageLength () {
+      return Math.ceil(this.total / 10)
+    },
+    nowPage: {
+      get() {
+        return this.pageNum
+      },
+      set(val) {
+        this.$emit("change", val);
+      }
+    },
     //计算属性，分页要显示的1和最后页中间的页数
-    pageList:function(){
+    pageList (){
       let list = [];
-      if(this.current > 3){ 
+      if(this.nowPage > 3){ 
         //如果当前页大于3，添加第二页和第三页
-        list.push(this.current-2)
-        list.push(this.current-1)
-        if(this.current > 4){
+        list.push(this.nowPage-2)
+        list.push(this.nowPage-1)
+        if(this.nowPage > 4){
             //如果当期页大于4，添加。。。
             this.ellipsis1 = true;
         }
-        if(this.current == 4){
+        if(this.nowPage == 4){
           //如果当前页=4，隐藏。。。
             this.ellipsis1 = false;
         }
       }else{
         //如果当前页小于3，隐藏。。。,添加第二页
         this.ellipsis1 = false;
-        for(let i = 2; i < this.current; i++){
+        for(let i = 2; i < this.nowPage; i++){
           list.push(i);
         }
       }
       //如果当前页不是第一页也不是最后一页，添加当前页
-      if(this.current != 1 && this.current != this.pageLength){
-        list.push(this.current);
+      if(this.nowPage != 1 && this.nowPage != this.pageLength){
+        list.push(this.nowPage);
       }
 
-      if(this.current < (this.pageLength - 2)){
+      if(this.nowPage < (this.pageLength - 2)){
         //如果当前页小于倒数第三页，添加当前页后两页
-        list.push(this.current+1);
-        list.push(this.current+2)
-        if(this.current < (this.pageLength - 3)){
+        list.push(this.nowPage+1);
+        list.push(this.nowPage+2)
+        if(this.nowPage < (this.pageLength - 3)){
           //如果当前页小于倒数第四页，显示。。。
           this.ellipsis2 = true;
         }
-        if(this.current == (this.pageLength - 3)){
+        if(this.nowPage == (this.pageLength - 3)){
           //如果当前页等于倒数第四页，隐藏。。。
           this.ellipsis2 = false;
         }
@@ -78,7 +94,7 @@ export default {
         //如果当前页大于等于倒数第三页，隐藏省略号
         this.ellipsis2 = false;
         //添加当前页到倒数第二页
-        for(let i = (this.current+1); i < this.pageLength; i++){
+        for(let i = (this.nowPage+1); i < this.pageLength; i++){
           list.push(i)
         }
       }
@@ -86,22 +102,17 @@ export default {
     }
   },
   methods:{
-    chosePage: function(num){
-      if(num != this.current){
-        this.current = num
+    chosePage (num){
+      if(num != this.nowPage){
+        this.nowPage = num
       }
     },
-    prePage: function(){
-      this.current --;
+    prePage (){
+      this.nowPage --;
     },
-    nextPage: function(){
-      this.current ++;
+    nextPage (){
+      this.nowPage ++;
     }
-  },
-  watch: {
-    current: function(val){
-      this.$emit("sendPage",val);
-    },
   }
 }
 </script>
